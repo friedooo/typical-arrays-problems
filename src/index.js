b5da17441827402a00fbd54662042e6d0c288db7
friedooo <friedlich3@gmail.com>
2020-02-25 18:39:42 +0300

    feat: complete test

--- src/index.js
+++ src/index.js
@@ -1,12 +1,27 @@
 
 exports.min = function min (array) {
-  return 0;
+    if (array == undefined || arguments[0] == undefined || array.length == 0 )
+    {
+        return 0;
+    } 
+  return Math.min.apply(null, array);
 }
 
 exports.max = function max (array) {
-  return 0;
+    if (array == undefined || arguments[0] == undefined || array.length == 0 )    {
+        return 0;
+    } 
+    return Math.max.apply(null, array);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (array == undefined || arguments[0] == undefined || array.length == 0 )    {
+        return 0;
+    } 
+    let sum = 0;
+    for (let elem of array)
+    {
+        sum += elem;
+    }
+  return sum/array.length;
 }
